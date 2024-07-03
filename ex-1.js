5f251e062ba9475d6ba12792977d821b4a35faf2
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-07-03 11:52:34 +0700

    feat: complete nested loop exercises (ex.1-3)

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,25 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printPattern() {
-   
+// ให้สร้าง Function printPattern Function นี้จะรับตัวเลขเข้ามา 1 ตัว แล้วแสดงผลลัพธ์ตามตัวเลขที่ได้รับมา เช่น ถ้าตัวเลขที่รับมาคือ 5 จะได้ผลลัพธ์ดังนี้
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+// เริ่มเขียนโค้ดตรงนี้
+function printPattern(number) {
+  let rowList = '';
+
+  for (let i = 0; i < number; i++) {
+    let row = '';
+
+    for (let j = 0; j <= i; j++) {
+      row += `${j + 1} `;
+    }
+
+    rowList += row + '\n';
+  }
+
+  return rowList;
 }
+
+console.log(printPattern(5));
